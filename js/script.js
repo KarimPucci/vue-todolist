41b8f5bbef6b27074140423135e0f46a1a348f23
Karim <kizpucci@gmail.com>
2022-11-04 17:31:44 +0100

    main

--- js/script.js
+++ js/script.js
@@ -9,7 +9,16 @@ createApp ({
                 {text: 'Fare benzina alla moto', done: true},
                 {text: 'Fare la spesa', done: false},
                 {text: 'Pagare la bolletta luce', done: false}
-            ]
+            ],
+            inputText:''
         }
     },
+    methods: {
+        createTask () {
+            this.todos.push({text:this.inputText,done:false})
+            if(this.inputText.lenght < 5){
+                this.error = 'Attenzione, il messaggio deve avere almeno 5 caratteri';
+            }
+        }
+    }
 }).mount('#app');
