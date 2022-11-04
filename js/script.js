77800de61bcb009e5b96681d0edea2f520932343
Karim <kizpucci@gmail.com>
2022-11-04 16:46:15 +0100

    main

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log("hello");
+
+const { createApp } = Vue;
+
+createApp ({
+    data(){
+        return{
+            todos: [
+                {text: 'Fare benzina alla moto', done: true},
+                {text: 'Fare la spesa', done: false},
+                {text: 'Pagare la bolletta luce', done: false}
+            ]
+        }
+    },
+    methods:{
+        
+    }
+}).mount('#app');
