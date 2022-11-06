c229aaabab0291335d9cf13fe2f2222087c1c3ee
Karim <kizpucci@gmail.com>
2022-11-06 13:05:30 +0100

    main

--- js/script.js
+++ js/script.js
@@ -10,14 +10,24 @@ createApp ({
                 {text: 'Fare la spesa', done: false},
                 {text: 'Pagare la bolletta luce', done: false}
             ],
-            inputText:''
+            inputText:'',
+            errorString:'',
         }
     },
     methods: {
         createTask () {
             this.todos.push({text:this.inputText,done:false})
             if(this.inputText.lenght < 5){
-                this.error = 'Attenzione, il messaggio deve avere almeno 5 caratteri';
+                this.errorString = 'Attenzione, il messaggio deve avere almeno 5 caratteri';
+            }
+        },
+        removeTask(index){
+            console.log(index);
+            this.errorString = '';
+            if(this.tasks[index].done){
+                this.tasks.splice(index,1);
+            }else{
+                this.errorString = 'attenzione! non si può eliminare il task se non è stato ancora fatto'
             }
         }
     }
